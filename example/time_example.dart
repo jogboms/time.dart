0750efb541109edd9274526ead23c25a510025d5
Brian Egan <brian@brianegan.com>
2019-10-14 16:37:38 +0200

    Add tests to the library

--- example/time_example.dart
+++ example/time_example.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:time/time.dart';
+
+void main() {
+  // Integer Extensions
+  print(1.weeks);
+  print(7.days);
+  print(22.hours);
+  print(45.minutes);
+  print(30.seconds);
+  print(15.milliseconds);
+  print(10.microseconds);
+  print(5.nanoseconds);
+
+  // DateTime Extensions
+  print(DateTime.now() + 7.days);
+  print(DateTime.now() - 7.days);
+
+  // Duration Extensions
+  print(7.days.inWeeks);
+  print(7.days.later);
+  print(7.days.ago);
+}
