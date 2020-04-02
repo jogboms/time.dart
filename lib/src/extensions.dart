c0e2ff97ce62f8e15d8f848a5251db03e92c6e6b
Jogboms <jeremiahogbomo@gmail.com>
2020-04-02 09:06:01 +0100

    Introduce copyWith to DateTime

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -60,6 +60,28 @@ extension DateTimeTimeExtension on DateTime {
       }
     }
   }
+
+  DateTime copyWith({
+    int year,
+    int month,
+    int day,
+    int hour,
+    int minute,
+    int second,
+    int millisecond,
+    int microsecond,
+  }) {
+    return DateTime(
+      year ?? this.year,
+      month ?? this.month,
+      day ?? this.day,
+      hour ?? this.hour,
+      minute ?? this.minute,
+      second ?? this.second,
+      millisecond ?? this.millisecond,
+      microsecond ?? this.microsecond,
+    );
+  }
 }
 
 extension DurationTimeExtension on Duration {
