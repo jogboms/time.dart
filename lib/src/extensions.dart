858e8ee3b452adab4721f48873f578f6c734260e
Daniil Kostin <dd@dgr.ru>
2021-05-08 08:26:09 +0300

    old formatting

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -15,17 +15,13 @@ extension NumTimeExtension<T extends num> on T {
   Duration get seconds => milliseconds * Duration.millisecondsPerSecond;
 
   /// Returns a Duration represented in milliseconds
-  Duration get milliseconds =>
-      Duration(
-          microseconds: (this * Duration.microsecondsPerMillisecond).toInt());
+  Duration get milliseconds => Duration(microseconds: (this * Duration.microsecondsPerMillisecond).toInt());
 
   /// Returns a Duration represented in microseconds
-  Duration get microseconds =>
-      milliseconds ~/ Duration.microsecondsPerMillisecond;
+  Duration get microseconds => milliseconds ~/ Duration.microsecondsPerMillisecond;
 
   /// Returns a Duration represented in nanoseconds
-  Duration get nanoseconds =>
-      microseconds ~/ DurationTimeExtension.nanosecondsPerMicrosecond;
+  Duration get nanoseconds => microseconds ~/ DurationTimeExtension.nanosecondsPerMicrosecond;
 }
 
 extension DateTimeTimeExtension on DateTime {
@@ -59,7 +55,7 @@ extension DateTimeTimeExtension on DateTime {
   /// Returns true if this year is a leap year.
   bool get isLeapYear =>
       // Leap years are used since 1582.
-  year >= 1582 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+      year >= 1582 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 
   /// Returns the amount of days that are in this month.
   ///
@@ -67,8 +63,7 @@ extension DateTimeTimeExtension on DateTime {
   int get daysInMonth {
     final days = [
       31, // January
-      if (isLeapYear) 29 else
-        28, // February
+      if (isLeapYear) 29 else 28, // February
       31, // March
       30, // April
       31, // May
@@ -149,9 +144,7 @@ extension DateTimeTimeExtension on DateTime {
 
   static int _calculateDifference(DateTime date) {
     final now = DateTime.now();
-    return DateTime(date.year, date.month, date.day)
-        .difference(DateTime(now.year, now.month, now.day))
-        .inDays;
+    return DateTime(date.year, date.month, date.day).difference(DateTime(now.year, now.month, now.day)).inDays;
   }
 
   /// Returns a range of dates to [to], exclusive start, inclusive end
@@ -160,8 +153,7 @@ extension DateTimeTimeExtension on DateTime {
   /// final end = DateTime(2020);
   /// start.to(end, by: const Duration(days: 365)).forEach(print); // 2020-01-01 00:00:00.000
   /// ```
-  Iterable<DateTime> to(DateTime to,
-      {Duration by = const Duration(days: 1)}) sync* {
+  Iterable<DateTime> to(DateTime to, {Duration by = const Duration(days: 1)}) sync* {
     if (isAtSameMomentAs(to)) return;
 
     if (isBefore(to)) {
